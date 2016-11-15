bdfe4f15c21cb7325a8d4a5ad1e83b0188645234
jens <jens.hirschfeld@keepout.de>
2016-11-15 21:44:19 +0100

    Added Chart.js to the header

--- src/WebApplication1/wwwroot/js/site.js
+++ src/WebApplication1/wwwroot/js/site.js
@@ -1,1 +1,13 @@
-﻿// Write your Javascript code.
+﻿function fehlerBehandlung(fehler, datei, zeile) {
+    alert("Fehler: " + fehler + "\nDatei: " + datei
+        + "\nZeile: " + zeile);
+}
+function textInCanvas(Headline) {
+    ctCustomer.beginPath();
+    ctCustomer.moveTo(0, 100);
+    ctCustomer.lineTo(400, 100);
+    ctCustomer.stroke();
+
+    ctCustomer.textBaseline = "alphabetic";
+    ctCustomer.fillText(Headline,10 ,100);
+}
