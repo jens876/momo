63bc1be033cce1313e831e4bca8b2bc72f7a9334
a101755 <user20@example.com>
2016-11-15 21:55:23 +0100

    Eventhandler eingebaut

--- src/WebApplication1/wwwroot/js/site.js
+++ src/WebApplication1/wwwroot/js/site.js
@@ -11,3 +11,11 @@ function textInCanvas(Headline) {
     ctCustomer.textBaseline = "alphabetic";
     ctCustomer.fillText(Headline,10 ,100);
 }
+function meinHandler(id, ereignis, funktion) {
+	if(window.addEventlistener)
+		document.getElementById(id)
+			.addEventListener(ereignis, funktion, false);
+	else if(window.attachEvent)
+		document.getElementById(id)
+			.attachEvent("on" + ereignis, funktion);
+}
