05b4741158ce5fcd1d0f87a0c53dfe9d330e6b22
a101755 <user20@example.com>
2016-11-16 14:10:21 +0100

    Lassen wir das mit den Ebenen.

--- src/WebApplication1/wwwroot/js/site.js
+++ src/WebApplication1/wwwroot/js/site.js
@@ -97,7 +97,7 @@ $(document)
 										lineTension : 0.1,
 										backgroundColor : "rgba(75,192,192,1)",
 										borderColor : "rgba(75,192,192,1)",
-										borderCapStyle : 'square',
+										borderCapStyle : 'butt',
 										borderDash : [],
 										borderDashOffset : 0.0,
 										borderJoinStyle : 'miter',
