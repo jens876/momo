2fe7f0df59aebe7dcffac39431f012b005e4c5d9
jens <jens.hirschfeld@keepout.de>
2016-11-16 12:32:04 +0100

    ChartData was missing

---
+++ src/WebApplication1/ChartData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class ChartData
+    {
+		public List<Double> accountBalances = new List<double>();
+		public List<Double> momoBalances = new List<double>();
+		public List<String> balanceLabels = new List<String>();
+	}
+}
