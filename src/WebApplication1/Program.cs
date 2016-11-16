0bc289560d8951d27427fa6f4a7abb8363829afc
user22 <xxx@starfinanz.de>
2016-11-16 09:47:08 +0100

    Open SQL-Connection on startup.

--- src/WebApplication1/Program.cs
+++ src/WebApplication1/Program.cs
@@ -4,16 +4,25 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using System.Net.Http;
+using MySql.Data.MySqlClient;
 
 namespace WebApplication1
 {
+
+    public static class StaticProperties
+    {
+        public static IHostingEnvironment m_hostingEnv;
+        public static MySqlConnection m_mySqlConnection;
+
+    }
+
     public class Program
     {
         public static void Main(string[] args)
         {
             var host = new WebHostBuilder()
                 .UseKestrel()
-				.UseUrls("http://0.0.0.0:8080/src/WebApplication1")
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
